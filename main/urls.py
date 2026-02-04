c8a23f25a753e41d193ebfbab2638992e72b79f3
Semfort <sementikhomay@gmail.com>
2026-02-04 12:40:46 +0300

    first

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('author/', views.author, name='author')
+]
---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1><a href='http://127.0.0.1:8000/author/'>Великий творец сайта</a>  <a href='http://127.0.0.1:8000/catalog/'>Страница магазина</a></h1>")
+
+def catalog(request):
+    return HttpResponse("<h1>Магазин спортивных товаров</h1>")
+
+def author(request):
+    return HttpResponse("<h1>Автор: Лоел Семён 89ТП</h1>")
